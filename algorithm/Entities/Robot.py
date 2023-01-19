e68328972be68e2f433da1c66ce2bd48d73d4af5
Wenyue Ma <c210114@e.ntu.edu.sg>
2023-01-19 23:20:14 +0800

    some controls

--- algorithm/Entities/Robot.py
+++ algorithm/Entities/Robot.py
@@ -1,23 +1,44 @@
-import math
 import pygame
 
-import settings
+from algorithm import settings
 
 
 class Robot:
     def __init__(self):
-        self.pos = (800, 800)
+        self.x = 0
+        self.y = 800
+        self.pos = (self.x, self.y)
         self.height = 300
         self.width = 300
         self.orientation = 90
         self.image = pygame.transform.scale(pygame.image.load("assets/car.png"), (6*settings.BLOCK_SIZE, 6*settings.BLOCK_SIZE))
+        self.car_rect = self.image.get_rect()
 
     def drawCar(self, SCREEN):
-        car_rect = self.image.get_rect()
-        car_rect.bottomleft = (0, 800)
-        SCREEN.blit(self.image, car_rect)
+        self.image = pygame.transform.rotate(self.image, self.orientation)
+        self.car_rect.bottomleft = (self.x,  self.y)
+        SCREEN.blit(self.image, self.car_rect)
         # rect = self.arrow.get_rect()
         # rect.bottomleft = (800,800)
         # self.SCREEN.blit(self.arrow, rect)
-    def collisonRadius(self):
-        pass
+    def moveStraight(self, StraightDistance):
+        if self.orientation == 90:
+            self.y = StraightDistance + self.y
+        elif self.orientation == 0:
+            self.x = StraightDistance + self.x
+        elif self.orientation == 180:
+            self.x = self.x - StraightDistance
+        else:
+            self.y = self.y - StraightDistance
+
+
+    def turning(self, turningDistance, orientation):
+        if orientation == 0 and self.orientation == 90:
+            self.orientation = 0
+            self.x = turningDistance + self.x
+            self.y = settings.TURNING_RADIUS + self.y
+        elif orientation == 180 and self.orientation == 90:
+            self.orientation = 180
+            self.x = self.x - turningDistance
+            self.y = self.y + settings.TURNING_RADIUS
+
--- algorithm/Entities/arena.py
+++ algorithm/Entities/arena.py
@@ -1,4 +1,5 @@
 import pygame
+from algorithm import settings
 """
 Represents the navigational area (default: 40x40 grid of 5x5cm grid cells)
 """
@@ -11,16 +12,17 @@ class Arena:
         self.obList = []
 
 
-    def drawGrid(self, SCREEN, WHITE, BLACK):
+    def drawGrid(self, SCREEN):
         RED = (251,0,0)
+
         for x in range(0,self.width, self.blockSize):
             for y in range(0, self.height, self.blockSize):
                 grid = pygame.Rect(x,y, self.blockSize, self.blockSize)
-                pygame.draw.rect(SCREEN, WHITE, grid, 1)
+                pygame.draw.rect(SCREEN, settings.WHITE, grid, 1)
         for obstacle in self.obstacles:
             ob = pygame.Rect(obstacle.pos, obstacle.dimension)
             self.obList.append(ob)
-            pygame.draw.rect(SCREEN, BLACK, ob)
+            pygame.draw.rect(SCREEN, settings.GREEN, ob)
             self.drawBorder(obstacle,SCREEN,RED,ob)
 
     def drawBorder(self, obstacle,  SCREEN, COLOUR, ob):
@@ -32,3 +34,8 @@ class Arena:
             pygame.draw.line(SCREEN, COLOUR, ob.bottomleft, ob.bottomright, 2)
         elif obstacle.imageOrientation == "left":
             pygame.draw.line(SCREEN, COLOUR, ob.topleft, ob.bottomleft, 2)
+
+    def updateGrid(self, robot, SCREEN):
+        SCREEN.fill((0,0,0))
+        self.drawGrid(SCREEN)
+        robot.drawCar(SCREEN)
