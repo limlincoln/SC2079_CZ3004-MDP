94c20860f6a9e8e73aaf9ebf132b25616a36f1ea
Wenyue Ma <c210114@e.ntu.edu.sg>
2023-01-27 09:48:28 +0800

    some configs

--- algorithm/algo/Astar.py
+++ algorithm/algo/Astar.py
@@ -1,8 +1,11 @@
 from algorithm.Entities import Obstacle
 class Astar:
-    def __init__(self, grid, obstacles: list[Obstacle]):
+    def __init__(self, grid, obstacles):
         #40x40 grid
         self.grid = grid
-
         self.obstacles = obstacles
+
+
+    def computePath(self):
+        pass
 
