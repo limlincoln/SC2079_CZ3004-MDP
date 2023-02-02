0cfd9259fdfccd0f0468790cdcd0aeea80dd1b4e
Wenyue Ma <c210114@e.ntu.edu.sg>
2023-02-03 01:19:18 +0800

    added aStar ( need to check for bugs)

--- algorithm/algo/test.py
+++ algorithm/algo/test.py
@@ -1,9 +1,12 @@
 import unittest
 import settings
+from constants import DIRECTION
 from algo.Dubins import Dubins
 from algo.Environment import StaticEnvironment
-from Astar import Astar
+from RRT import RRT
 from Entities.Obstacle import Obstacle
+from Astar import Astar
+
 class SimSum(unittest.TestCase):
 
     def testDubins(self):
@@ -33,7 +36,7 @@ class SimSum(unittest.TestCase):
         test = [Obstacle((40, 20), "right", (2 * settings.BLOCK_SIZE, 2 * settings.BLOCK_SIZE)),
                 Obstacle((70, 70), "top", (2 * settings.BLOCK_SIZE, 2 * settings.BLOCK_SIZE))]
         env = StaticEnvironment((100, 100), test)
-        rrt = Astar(env)
+        rrt = RRT(env)
 
         start = env.randomFreeSpace()
         end = env.randomFreeSpace()
@@ -42,6 +45,18 @@ class SimSum(unittest.TestCase):
         rrt.computePath(end, 200, metric='local')
         rrt.getPath(True)
         print('Static ok')
+    def testAstar(self):
+        """
+        Tests the Astar
+        :return:
+        """
+
+        test = [Obstacle((40, 20), "right", (2 * settings.BLOCK_SIZE, 2 * settings.BLOCK_SIZE)),
+                Obstacle((70, 70), "top", (2 * settings.BLOCK_SIZE, 2 * settings.BLOCK_SIZE))]
+        env = StaticEnvironment((100, 100), test)
+        aStar = Astar(env, (0,0, DIRECTION.TOP, 'P'), env.generateTargetLocation()[0])
+
+        print("Astar OK")
 
 
 if __name__ == '__main__':
