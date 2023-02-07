5c05a37f5a9acbc89f5a1cc49eedffb20db0eec3
C190194 <cblxx11@gmail.com>
2023-02-07 10:25:46 +0000

    RPi threads added

---
+++ RPI/Multithreading/bluetooth_thread.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""PyBluez simple example rfcomm-server.py
+Simple demonstration of a server application that uses RFCOMM sockets.
+Author: Albert Huang <albert@csail.mit.edu>
+$Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
+"""
+
+# Command
+# cd /home/RP03/Desktop/Python\ Test
+# python bluetooth_thread.py 
+
+# To get uuid
+# sudo blkid
+
+# Somehow fixed the bluetooth permission denied error
+# sudo chmod o+rw /var/run/sdp
+
+import os
+import bluetooth
+import threading
+
+from config import *
+
+
+
+class Connect_Android_Client(threading.Thread):
+
+    def __init__(self, name, uuid, port, shoot_signal):
+        super(Connect_Android_Client, self).__init__()
+        self.name = name
+        self.uuid = uuid
+        self.port = port
+        self.shoot_signal = shoot_signal
+        #self.results = results
+        
+        # make RPi discoverable
+        os.system("sudo hciconfig hci0 piscan")
+
+        self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.server_sock.bind(("", self.port))
+        self.server_sock.listen(1)
+
+        port = self.server_sock.getsockname()[1]
+        
+        bluetooth.advertise_service(self.server_sock, "SampleServer", service_id=uuid,
+                                    service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                                    profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                                    # protocols=[bluetooth.OBEX_UUID]
+                                    )
+
+        print("Waiting for connection on RFCOMM channel", port)
+    
+    def run(self):
+        client_sock, client_info = self.server_sock.accept()
+        print("Accepted connection from", client_info)
+
+        try:
+            while True:
+                data = client_sock.recv(1024)
+                if not data:
+                    break
+                s = data.decode('UTF-8').strip()
+                print("Received", s)
+                # process the received string
+                if s == "end":
+                    break
+                elif s.startswith("tp"):
+                    img_map_id = s.split("tp")[-1]
+                    self.shoot_signal.put(img_map_id)
+                    print("Take Picture Command sent")
+        except OSError:
+            pass
+
+        print("Disconnected.")
+
+        client_sock.close()
+        self.server_sock.close()
+        print("All done.")
+        
+        
+if __name__ == '__main__':
+    shoot_signal = Queue(maxsize=1)
+    
+    BLE_thread = Connect_Android_Client("BLE Thread", UUID, BLE_PORT, shoot_signal)
+    BLE_thread.start()
+    BLE_thread.join()
+    print("Bluetooth thread ended")
+
+
+
+    
+    
---
+++ RPI/Multithreading/main_thread.py
@@ -0,0 +1,72 @@
+#import socket
+import threading
+from queue import Queue
+import cv2
+import pickle
+import struct
+import time
+import os
+#import io
+#import zlib
+#from signal import signal, SIGPIPE, SIG_DFL
+#import picamera
+import numpy as np
+
+from config import *
+from PC_thread import *
+from bluetooth_thread import *
+
+
+# Command
+# cd /home/RP03/Desktop/Python\ Test/SC2079_CZ3004-MDP/RPI/Multithreading
+# python main_thread.py 
+
+if __name__ == '__main__':
+    if not os.path.exists("image_result"):
+        os.makedirs("image_result")
+    
+    # Create Queue objects for tranferring data among threads
+    img_results = Queue(maxsize=1)
+    shoot_signal = Queue(maxsize=1)
+    PC_close_signal = Queue(maxsize=1)
+    
+    # Start threads
+    PC_thread = Connect_PC_Client("PC Thread", img_results, shoot_signal, PC_close_signal)
+    PC_thread.start()
+    BLE_thread = Connect_Android_Client("BLE Thread", UUID, BLE_PORT, shoot_signal)
+    BLE_thread.start()
+    time.sleep(5)
+    
+    # show current img recog results
+    for i in range(3):
+        while img_results.empty():
+            time.sleep(0.1)
+        res = img_results.get()
+        print("Image Rec Results:", res)
+    PC_close_signal.put(1)
+    
+    # Wait for threads to end
+    PC_thread.join()
+    BLE_thread.join()
+    
+    
+    
+    
+    '''
+    for i in range(3):
+        t1 = time.time()
+        # send the command to take picture
+        shoot_signal.put(i) # param: the img id on the map
+        # show current img recog results
+        while img_results.empty():
+            time.sleep(0.1)
+        res = img_results.get()
+        t2 = time.time()
+        print("Image Rec Results:", res)
+        print("Time taken:", t2-t1)
+        print()
+        time.sleep(3)
+    PC_close_signal.put(1)
+    PC_thread.join()
+    '''
+
